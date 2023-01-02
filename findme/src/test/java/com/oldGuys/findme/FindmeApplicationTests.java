794421d111b8fd3187e109dc1aab41d0b61cd8f9
Seonghun Park <88868293+seonghun0@users.noreply.github.com>
2023-01-02 11:44:31 +0900

    Add files via upload
    install commit

---
+++ findme/src/test/java/com/oldGuys/findme/FindmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oldGuys.findme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
